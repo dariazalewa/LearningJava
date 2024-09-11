ea07189b93b4cb54cdf9498b39749bd101f0992c
dariazalewa <94965342+dariazalewa@users.noreply.github.com>
2024-09-11 13:02:17 +0200

    Delete Operators.java

--- Operators.java
+++
@@ -1,9 +0,0 @@
-public class Operators {
-    public static void main(String[] args) {
-      System.out.println(3 + 3);
-      System.out.println(3 - 3);
-      System.out.println(3 * 3);
-      System.out.println(3 / 3);
-      System.out.println(3 % 3);
-    }
-  }  
